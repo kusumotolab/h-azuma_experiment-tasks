56bd8fae4120622c8f1b246151c66677a16d06fb
h-azuma <h-azuma@ist.osaka-u.ac.jp>
2019-07-11 23:49:22 +0900

    add two tasks

---
+++ src/control_list/Task1_1.java
@@ -0,0 +1,29 @@
+package control_list;
+
+// AtCoder Beginner Contest 009 - Bを改題
+// https://atcoder.jp/contests/abc009/tasks/abc009_2
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Task1_1 {
+    public static void main(String[] args){
+        // prepare an integer list
+        List<Integer> list = Arrays.asList(1200, 300, 1200, 500, 800);
+
+        int second = 0;
+        int max = 0;
+
+        for(int num : list){
+            if(num > max){
+                // update max and second
+                second = max;
+                max = num;
+            }else if(num > second && num < max){
+                second = num;
+            }
+        }
+
+        System.out.println(second);
+    }
+}
---
+++ src/control_string/Task2_1.java
@@ -0,0 +1,24 @@
+package control_string;
+
+// AtCoder Beginner Contest 002 - Bを改題
+// https://atcoder.jp/contests/abc002/tasks/abc002_2
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task2_1 {
+    public static void main(String[] args){
+        String str = "amkllannsnom";
+
+        // store the character of the string one by one
+        List<String> stringList = new ArrayList<String>(Arrays.asList(str.split("")));
+
+        stringList.removeIf(s -> s.matches("l|m|n"));
+
+        // output the remained string
+        for(int i = stringList.size() - 1; i >= 0; i--){
+            System.out.print(stringList.get(i));
+        }
+    }
+}
