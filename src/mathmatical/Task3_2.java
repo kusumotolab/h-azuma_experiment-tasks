5e34d522582e1936abbef285209f85e151e7a88a
h-azuma <h-azuma@ist.osaka-u.ac.jp>
2019-07-12 14:53:02 +0900

    add task3-2

---
+++ src/mathmatical/Task3_2.java
@@ -0,0 +1,24 @@
+package mathmatical;
+
+// AtCoder Beginner Contest 006 - Cを改題
+// https://atcoder.jp/contests/abc006/tasks/abc006_3
+
+public class Task3_2 {
+    public static void main(String[] args){
+        int sum = 10;
+        int legs = 36;
+
+        int crane = 0;
+        int turtle = sum;
+
+        // crane has 2 legs and turtle has 4 legs
+        while(!(2 * crane + 4 * turtle == legs && crane + turtle == sum)){
+            // Reduce turtle number and add crane number
+            crane++;
+            turtle--;
+        }
+
+        System.out.println("crane: " + crane);
+        System.out.println("turtle: " + turtle);
+    }
+}
