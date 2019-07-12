98da65a65c2b2616c9f04b42ce0060e4fb7d12fc
h-azuma <h-azuma@ist.osaka-u.ac.jp>
2019-07-12 12:14:33 +0900

    add tasks

---
+++ src/control_string/Task2_2.java
@@ -0,0 +1,44 @@
+package control_string;
+
+// AtCoder Beginner Contest 007 - Bを改題
+// https://atcoder.jp/contests/abc007/tasks/abc007_2
+
+public class Task2_2 {
+    public static void main(String[] args){
+        char ascii = 65; // "65" is "A"
+        String[] character = new String[26];
+
+        // set from "A" to "Z" to character[]
+        for(int i = 0; i < character.length; i++){
+            character[i] = String.valueOf((char)(ascii + i));
+        }
+
+        String str1 = "OSUSHI";
+        String str2 = "OSECHI";
+
+        String[] str1Array = str1.split("");
+        String[] str2Array = str2.split("");
+
+        System.out.print("str1 is ");
+        for(int i = 0; i < str1Array.length; i++){
+            int str1Num = -1;
+            int str2Num = -1;
+            for(int j = 0; j < character.length; j++){
+                if(str1Array[i].equals(character[j])){
+                    str1Num = j;
+                }
+                if(str2Array[i].equals(character[j])){
+                    str2Num = j;
+                }
+            }
+            if(str1Num > str2Num){
+                System.out.print("behind str2 ");
+                break;
+            }else if(str1Num < str2Num){
+                System.out.print("in front of str2 ");
+                break;
+            }
+        }
+        System.out.println("in dictionary.");
+    }
+}
---
+++ src/mathmatical/Task3_1.java
@@ -0,0 +1,32 @@
+package mathmatical;
+
+// AtCoder Beginner Contest 006 - Bを改題
+// https://atcoder.jp/contests/abc006/tasks/abc006_2
+
+public class Task3_1 {
+    public static void main(String args[]){
+        // followings are initial numbers
+        int a1 = 0;
+        int a2 = 0;
+        int a3 = 1;
+
+        // tmp is a temporary variable
+        int tmp;
+        int an = 0;
+
+        for(int i = 0; i < 5; i++){
+            tmp = a1 + a2 + a3;
+
+            if(i + 1 >= 5){
+                an = tmp;
+                break;
+            }
+
+            a1 = a2;
+            a2 = a3;
+            a3 = tmp;
+        }
+
+        System.out.println(an);
+    }
+}
