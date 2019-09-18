dd44dd01af75868a65487430d7624c6950a26221
h-azuma <h-azuma@ist.osaka-u.ac.jp>
2019-09-18 09:06:51 +0900

    add task

---
+++ src/mathmatical/Task3_9.java
@@ -0,0 +1,27 @@
+package mathmatical;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Task3_9 {
+  public static void main(String[] args) {
+    // prime number is the number isn't divisible without 1 and itself
+    final List<Integer> primeNumbers = Arrays.asList(2, 3, 5, 7, 11, 13, 17);
+
+    int target = 220;
+    int i = 0;
+    int count = 0;
+
+    while (target != 1) {
+      if (target % primeNumbers.get(i) == 0) {
+        target /= primeNumbers.get(i);
+        count++;
+      } else {
+        // look at next prime number
+        i++;
+      }
+    }
+
+    System.out.println(count);
+  }
+}
